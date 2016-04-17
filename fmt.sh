dbacc522911226a272f327af34dd1c0e2f776d79
Pierre Fenoll <pierrefenoll@gmail.com>
2016-04-18 01:07:17 +0200

    fmt.sh: easier CLI interface

--- fmt.sh
+++ fmt.sh
@@ -2,11 +2,22 @@
 
 # Format Erlang code using Emacs' erlang-mode from github.com/erlang/otp
 
+[[ $# -eq 0 ]] && echo "Usage: $0  <path to .erl or folder>+" && exit 1
+
 curdir="$(dirname "$0")"
 
 EMACS=${EMACS:-emacs}
 ERLANGEL="${ERLANGEL:-$curdir/emacs}"
 
-echo .$ERLANGEL.
+function format() {
+    $EMACS --batch --quick --directory "$ERLANGEL" --load "$curdir"/fmt.el $*
+}
 
-$EMACS --batch --quick --directory "$ERLANGEL" --load "$curdir"/fmt.el $*
+while [[ "$1" != '' ]]; do
+    if [[ -d "$1" ]]; then
+        format $(find "$1" -name *.erl)
+    else
+        format "$1"
+    fi
+    shift
+done
