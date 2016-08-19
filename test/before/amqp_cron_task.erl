c6a56f5031589c30ad6f642f6ede72d21718c5be
Pierre Fenoll <pierrefenoll@gmail.com>
2016-08-19 23:36:20 +0200

    issue3: bug related to line break between `fun` and `(`

---
+++ test/before/amqp_cron_task.erl
@@ -0,0 +1,13 @@
+-module(amqp_cron_task).
+
+-export([extract_integers/3]).
+
+extract_integers(Min, Max, Acc) ->
+    fun
+        (Int) when Int < Min ->
+                                            throw({'error', {'out_of_range', {'min', Min}, {'value', Int}}});
+        (Int) when Int > Max ->
+                                            throw({'error', {'out_of_range', {'max', Max}, {'value', Int}}});
+        (_Int) ->
+                                            'ok'
+                                    end.
