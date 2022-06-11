baa2e93acc1d40e2aa0560f59aaca6187f34e3ce
Pierre Fenoll <pierrefenoll@gmail.com>
2022-06-11 12:11:43 +0200

    skip (erlang-mode)
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- priv/fmt.el
+++ priv/fmt.el
@@ -7,7 +7,7 @@
 (defun fmt-file (arg)
   (message "fmt %s" arg)
   (find-file arg)
-  (erlang-mode)
+  ;; (erlang-mode)
   ;; (toggle-debug-on-error)
   (untabify (point-min) (point-max))
   (condition-case ex
